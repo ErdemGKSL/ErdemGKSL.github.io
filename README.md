6c151c883d1ca190582414614065bee931e88e80
Erdem <74366943+ErdemGKSL@users.noreply.github.com>
2022-10-07 03:25:03 +0300

    yey

--- README.md
+++
@@ -1,1 +0,0 @@
-# ErdemGKSL.github.io
---
+++ index.js
@@ -0,0 +1,2 @@
+const aboutMe = document.getElementById("replacer-0");
+aboutMe.textContent = aboutMe.textContent.replace("{0}", ((Date.now() - 1622386800000) / (1000 * 60 * 60 * 24)).toLocaleString({}, { maximumFractionDigits: 2 }));
