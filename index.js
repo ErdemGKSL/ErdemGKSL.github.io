a7c4d5f214a37c39dabe58dce802757a5213e786
Erdem <74366943+ErdemGKSL@users.noreply.github.com>
2022-10-20 20:05:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const aboutMe = document.getElementById("replacer-0");
-aboutMe.textContent = aboutMe.textContent.replace("{0}", ((Date.now() - 1622386800000) / (1000 * 60 * 60 * 24)).toLocaleString({}, { maximumFractionDigits: 2 }));
+aboutMe.textContent = aboutMe.textContent.replace("{0}", ((Date.now() - 1622386800000) / (1000 * 60 * 60 * 24 * 30)).toLocaleString({}, { maximumFractionDigits: 2 }));
